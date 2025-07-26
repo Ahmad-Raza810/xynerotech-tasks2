e88acabe8e39a2e63e5281e73885feb941fd8073
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-26 16:15:14 +0530

    adding api enpoints.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/controller/UserController.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.xynerotech.task.household_services_booking_platform.controller;
 
+import com.xynerotech.task.household_services_booking_platform.entities.User;
+import com.xynerotech.task.household_services_booking_platform.service.UserService;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Data
+@RestController
+@RequestMapping("api/user")
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    //api endpoint for adding new user.
+
+    @GetMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User returnedUser=userService.addUser(user);
+       return  new ResponseEntity<>(returnedUser, HttpStatus.CREATED);
+    }
 }
