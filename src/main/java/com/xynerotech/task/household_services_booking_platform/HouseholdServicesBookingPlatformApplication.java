3589aff08c4f261755f8add82d342bed6c3b25be
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-26 16:16:48 +0530

    Main class of execution.

---
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/HouseholdServicesBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.xynerotech.task.household_services_booking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdServicesBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdServicesBookingPlatformApplication.class, args);
+	}
+
+}
