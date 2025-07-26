b819f8aabdb1673401ae633b26beec1919837093
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-26 16:19:27 +0530

    adding  service method impl.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/service/UserServiceImpl.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/service/UserServiceImpl.java
@@ -2,9 +2,8 @@ package com.xynerotech.task.household_services_booking_platform.service;
 
 import com.xynerotech.task.household_services_booking_platform.entities.User;
 import com.xynerotech.task.household_services_booking_platform.exception.ResourceNotFoundException;
-import com.xynerotech.task.household_services_booking_platform.response.UserRepository;
+import com.xynerotech.task.household_services_booking_platform.repository.UserRepository;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
