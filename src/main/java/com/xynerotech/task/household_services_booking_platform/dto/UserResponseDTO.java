67cfd01dfeecd0f6381cf24f30b54fa924f05665
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-29 23:04:27 +0530

    user response dto added.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/UserResponseDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/UserResponseDTO.java
@@ -1,4 +1,27 @@
 package com.xynerotech.task.household_services_booking_platform.dto;
 
+import com.xynerotech.task.household_services_booking_platform.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDTO {
+    private Long userId;
+    private String userName;
+    private String email;
+    private String role;
+
+
+    public static UserResponseDTO userToResponseDto(User user){
+        UserResponseDTO responseDTO=new UserResponseDTO();
+        responseDTO.setUserId(user.getUserId());
+        responseDTO.setUserName(user.getUserName());
+        responseDTO.setEmail(user.getEmail());
+        responseDTO.setRole(user.getRole());
+
+        return responseDTO;
+    }
 }
