e580e2177f7faf9df8f1c3c6b72cd1215d9ee896
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-29 23:04:52 +0530

    create user dto added.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/CreateUserDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/CreateUserDTO.java
@@ -1,4 +1,40 @@
 package com.xynerotech.task.household_services_booking_platform.dto;
 
+import com.xynerotech.task.household_services_booking_platform.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserDTO {
+    @NotBlank(message = "Username is required")
+    private String userName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+
+    public static User dtoToUser(CreateUserDTO createUserDTO){
+        User user=new User();
+        user.setUserName(createUserDTO.getUserName());
+        user.setEmail(createUserDTO.getEmail());
+        user.setPassword(createUserDTO.getPassword());
+        return user;
+    }
+
+    public static CreateUserDTO userToDto(User user){
+        CreateUserDTO createUserDTO=new CreateUserDTO();
+        createUserDTO.setUserName(user.getUserName());
+        createUserDTO.setEmail(user.getEmail());
+        createUserDTO.setPassword(user.getPassword());
+        return createUserDTO;
+    }
 }
