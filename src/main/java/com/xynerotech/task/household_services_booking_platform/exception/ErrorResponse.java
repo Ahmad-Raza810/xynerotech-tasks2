98efefd4c2b7ff12743ebfac3853c38e4de2e799
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-29 11:20:03 +0530

    errorresponse bug fixed.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/exception/ErrorResponse.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/exception/ErrorResponse.java
@@ -1,4 +1,15 @@
 package com.xynerotech.task.household_services_booking_platform.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
 public class ErrorResponse {
+    private String message;
+    private LocalDateTime timestamp;
+    private int statuscode;
 }
