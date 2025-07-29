f5e31c219649c573e2f70aaa6be0fc5b29a4871f
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-29 19:13:03 +0530

    dto added for UserUpdate.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/UserUpdateDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/UserUpdateDTO.java
@@ -1,4 +1,48 @@
-package com.xynerotech.task.household_services_booking_platform.dto;
+    package com.xynerotech.task.household_services_booking_platform.dto;
 
-public class UserUpdateDTO {
-}
+    import com.xynerotech.task.household_services_booking_platform.entities.User;
+    import com.xynerotech.task.household_services_booking_platform.validation.AtLeastOneFieldNotEmpty;
+    import jakarta.validation.constraints.Email;
+    import jakarta.validation.constraints.NotBlank;
+    import lombok.AllArgsConstructor;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @AtLeastOneFieldNotEmpty(message = "At least one field (userName, email, or password) must be provided.")
+    public class UserUpdateDTO {
+
+
+        private String userName;
+
+        private Long userId;
+
+        private String role;
+
+        @Email(message = "Invalid email format.")
+        private String email;
+
+        private String password;
+
+        public static User dtoToUser(UserUpdateDTO userUpdateDTO){
+            User user=new User();
+            user.setUserName(userUpdateDTO.getUserName());
+            user.setEmail(userUpdateDTO.getEmail());
+            user.setPassword(userUpdateDTO.getPassword());
+            return user;
+        }
+
+        public static UserUpdateDTO userToDto(User user){
+            UserUpdateDTO userUpdateDTO=new UserUpdateDTO();
+            userUpdateDTO.setUserId(user.getUserId());
+            userUpdateDTO.setUserName(user.getUserName());
+            userUpdateDTO.setEmail(user.getEmail());
+            userUpdateDTO.setPassword(user.getPassword());
+            userUpdateDTO.setRole(user.getRole());
+            return userUpdateDTO;
+        }
+    }
+
+
