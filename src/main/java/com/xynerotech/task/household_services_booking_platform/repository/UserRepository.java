bca6a48d1bfc2915fa8c8051c33e4dfd714cfd83
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-31 20:09:47 +0530

    findbyemail method added.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/repository/UserRepository.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.xynerotech.task.household_services_booking_platform.repository;
 
-import com.xynerotech.task.household_services_booking_platform.entities.User;
+import com.xynerotech.task.household_services_booking_platform.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<AppUser,Long> {
+    Optional<AppUser> findByEmail(String email);
+
 }
