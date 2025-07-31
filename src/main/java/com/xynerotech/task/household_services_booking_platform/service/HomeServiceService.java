bcbbbf63bdff377322755b4e85355cbb1dadc1ba
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-31 23:44:01 +0530

    method sign for HomeService.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/service/HomeServiceService.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/service/HomeServiceService.java
@@ -1,4 +1,13 @@
 package com.xynerotech.task.household_services_booking_platform.service;
 
+import com.xynerotech.task.household_services_booking_platform.entities.HomeService;
+
+import java.util.List;
+
 public interface HomeServiceService {
+    List<HomeService> getAllServices();
+    HomeService getServiceById(Long id);
+    HomeService addService(HomeService service);
+    HomeService updateService(Long id, HomeService updatedService);
+    void deleteService(Long id);
 }
