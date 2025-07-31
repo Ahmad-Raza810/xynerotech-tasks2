57284e5f87c7e9814825be3013712637dafafe8d
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-31 23:42:23 +0530

    validation added in HomeService.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/entities/HomeService.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/entities/HomeService.java
@@ -1,20 +1,30 @@
 package com.xynerotech.task.household_services_booking_platform.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Service {
+public class HomeService {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Service name is required")
+    @Size(min = 3, max = 50, message = "Service name must be between 3 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Service description is required")
+    @Size(min = 10, max = 255, message = "Description must be between 10 and 255 characters")
     private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private Double price;
-
-    // Constructors, Getters, Setters
 }
-
