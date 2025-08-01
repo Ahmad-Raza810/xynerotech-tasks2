a95dd926f86fef9f1a0a6a1eef9d569500f2c275
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-01 10:42:07 +0530

    making class generic.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/validation/AtLeastOneFieldValidator.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/validation/AtLeastOneFieldValidator.java
@@ -1,16 +1,25 @@
 package com.xynerotech.task.household_services_booking_platform.validation;
 
-import com.xynerotech.task.household_services_booking_platform.dto.UserUpdateDTO;
+import com.xynerotech.task.household_services_booking_platform.dto.UpdateHomeServiceDTO;
+import com.xynerotech.task.household_services_booking_platform.dto.UpdateUserDTO;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.util.StringUtils;
 
-public class AtLeastOneFieldValidator implements ConstraintValidator<AtLeastOneFieldNotEmpty, UserUpdateDTO> {
+public class AtLeastOneFieldValidator implements ConstraintValidator<AtLeastOneFieldNotEmpty, Object> {
 
     @Override
-    public boolean isValid(UserUpdateDTO userUpdateDTO, ConstraintValidatorContext constraintValidatorContext) {
-        return StringUtils.hasText(userUpdateDTO.getUserName()) ||
-                StringUtils.hasText(userUpdateDTO.getEmail()) ||
-                StringUtils.hasText(userUpdateDTO.getPassword());
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (value instanceof UpdateUserDTO dto) {
+            return StringUtils.hasText(dto.getUserName()) ||
+                    StringUtils.hasText(dto.getEmail()) ||
+                    StringUtils.hasText(dto.getPassword());
+        } else if (value instanceof UpdateHomeServiceDTO dto) {
+            return StringUtils.hasText(dto.getName()) ||
+                    StringUtils.hasText(dto.getDescription()) ||
+                    dto.getPrice() != null;
+        }
+
+        return false; // unsupported object type
     }
 }
