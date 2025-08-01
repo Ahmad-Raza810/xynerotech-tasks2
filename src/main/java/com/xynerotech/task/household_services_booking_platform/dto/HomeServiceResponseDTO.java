df86a270af3a78310e4f2d335c911a0d770425ea
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-01 10:39:41 +0530

    including lombok .

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/HomeServiceResponseDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/HomeServiceResponseDTO.java
@@ -1,4 +1,26 @@
 package com.xynerotech.task.household_services_booking_platform.dto;
 
+import com.xynerotech.task.household_services_booking_platform.entities.HomeService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HomeServiceResponseDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public static HomeServiceResponseDTO userToDto(HomeService homeService) {
+        HomeServiceResponseDTO dto = new HomeServiceResponseDTO();
+        dto.setId(homeService.getId());
+        dto.setName(homeService.getName());
+        dto.setDescription(homeService.getDescription());
+        dto.setPrice(homeService.getPrice());
+        return dto;
+    }
 }
