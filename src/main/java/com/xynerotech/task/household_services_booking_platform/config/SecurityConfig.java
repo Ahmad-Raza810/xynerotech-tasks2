bbf7323e60bfe79af6265b05b48cc9faa1d2338a
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-02 18:37:21 +0530

    securityconfig class.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/config/SecurityConfig.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/config/SecurityConfig.java
@@ -1,4 +1,49 @@
-package com.xynerotech.task.household_services_booking_platform.controller;
+package com.xynerotech.task.household_services_booking_platform.config;
 
+import com.xynerotech.task.household_services_booking_platform.security.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/health",
+                                "/api/register",
+                                "/api/login",
+                                "/api/service/get",
+                                "/api/service/get/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        // 🔐 ADMIN only routes
+                        .requestMatchers("/api/users/**", "/api/service/**").hasRole("ADMIN")
+                        // 🧑‍ USER-only routes (optional)
+                        //.requestMatchers("/api/bookings/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
 }
