4db5a062572ad77aa738044c6812a1ac95ef043c
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-02 18:55:27 +0530

    add userrepo.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/repository/UserRepository.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.xynerotech.task.household_services_booking_platform.repository;
 
 import com.xynerotech.task.household_services_booking_platform.entities.AppUser;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<AppUser,Long> {
     Optional<AppUser> findByEmail(String email);
 
+    boolean existsByEmail(@Email(message = "Invalid email format") @NotBlank(message = "Email is required") String email);
 }
