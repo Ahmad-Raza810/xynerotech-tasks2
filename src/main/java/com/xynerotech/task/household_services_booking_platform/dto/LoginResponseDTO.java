0a9497643ca21a945a078ffb5617b212abcb55fd
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-02 18:48:46 +0530

    adding LoginResponseDTO.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/LoginResponseDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/LoginResponseDTO.java
@@ -1,4 +1,12 @@
 package com.xynerotech.task.household_services_booking_platform.dto;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponseDTO {
+    private String token;
+    private String email;
+    private String role;
 }
