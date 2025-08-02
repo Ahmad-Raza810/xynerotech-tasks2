c74d584bcda817cf6169b2e4f9f017353a1ee5c6
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-02 18:48:29 +0530

    adding LoginRequestDTO.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/LoginRequestDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/LoginRequestDTO.java
@@ -1,4 +1,18 @@
 package com.xynerotech.task.household_services_booking_platform.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
+
