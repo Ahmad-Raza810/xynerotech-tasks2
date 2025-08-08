bdaeef66ecbbb191c79dfbb6a4507463a96706a5
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-08 22:38:59 +0530

    interface for booking service.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingService.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingService.java
@@ -1,4 +1,11 @@
 package com.xynerotech.task.household_services_booking_platform.service;
 
+import com.xynerotech.task.household_services_booking_platform.dto.bookingDTO.CreateBookingDTO;
+import com.xynerotech.task.household_services_booking_platform.entities.Booking;
+
 public interface BookingService {
+
+    Booking addBooking(CreateBookingDTO bookingDTO);
+
+
 }
