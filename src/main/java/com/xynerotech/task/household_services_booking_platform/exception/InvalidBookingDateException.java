909ce927be80e7c09ddb538fe5627b5181b967ad
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-08 22:40:43 +0530

    custom exception for invalid date.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/exception/InvalidBookingDateException.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/exception/InvalidBookingDateException.java
@@ -1,7 +1,7 @@
 package com.xynerotech.task.household_services_booking_platform.exception;
 
 public class InvalidBookingDateException extends RuntimeException {
-  public InvalidBookingDateException(String message) {
-    super(message);
-  }
+    public InvalidBookingDateException(String message) {
+        super(message);
+    }
 }
