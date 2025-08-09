62729b1c31395fb0a8f4d4bf582a46eb2713e384
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-09 09:02:28 +0530

    completed impl of add a booking.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingServiceImpl.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingServiceImpl.java
@@ -5,23 +5,28 @@ import com.xynerotech.task.household_services_booking_platform.entities.AppUser;
 import com.xynerotech.task.household_services_booking_platform.entities.Booking;
 import com.xynerotech.task.household_services_booking_platform.entities.BookingStatus;
 import com.xynerotech.task.household_services_booking_platform.entities.HomeService;
+import com.xynerotech.task.household_services_booking_platform.exception.BookingConflictException;
 import com.xynerotech.task.household_services_booking_platform.exception.InvalidBookingDateException;
 import com.xynerotech.task.household_services_booking_platform.exception.ResourceNotFoundException;
 import com.xynerotech.task.household_services_booking_platform.repository.BookingRepository;
 import com.xynerotech.task.household_services_booking_platform.repository.HomeServiceRepository;
 import com.xynerotech.task.household_services_booking_platform.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 
+@Slf4j
 @Service
+@Transactional
 public class BookingServiceImpl implements BookingService{
 
     @Autowired
-    private BookingRepository bookingServiceRepository;
+    private BookingRepository bookingRepository;
 
     @Autowired
     private HomeServiceRepository homeServiceRepository;
@@ -32,7 +37,6 @@ public class BookingServiceImpl implements BookingService{
 
 
     @Override
-    @Transactional
     public Booking addBooking(CreateBookingDTO bookingDTO) {
 
         //checking user exists or not.
@@ -46,9 +50,24 @@ public class BookingServiceImpl implements BookingService{
                .orElseThrow(()->new ResourceNotFoundException("service with id '"+bookingDTO.getServiceId()+"' not exists."));
 
        //checking date is not from past.
-        if (bookingDTO.getDate().isBefore(LocalDate.now())) {
+        ZoneId userZone = ZoneId.of("Asia/Kolkata");
+        LocalDate todayInUserZone = LocalDate.now(userZone);
+
+        // checking user have same booking(same service and user)   at same date or not.
+        if (bookingDTO.getDate().isBefore(todayInUserZone)) {
             throw new InvalidBookingDateException("Date must not be in the past");
         }
+
+        if(bookingRepository.existsByUserAndServiceAndBookingDate(user,service,bookingDTO.getDate()))
+        {
+            throw new BookingConflictException("You have already following booking at same date you mentioned.");
+        }
+
+
+        log.info("Creating booking for userId={}, serviceId={}, date={}",
+                bookingDTO.getUserId(),
+                bookingDTO.getServiceId(),
+                bookingDTO.getDate());
 
 
         //creating booking object and setting up values
@@ -58,7 +77,6 @@ public class BookingServiceImpl implements BookingService{
         booking.setBookingDate(bookingDTO.getDate());
         booking.setStatus(BookingStatus.PENDING);
 
-       return  bookingServiceRepository.save(booking);
-
+       return  bookingRepository.save(booking);
     }
 }
