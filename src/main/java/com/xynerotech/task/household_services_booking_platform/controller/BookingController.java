f36d429a9b4d3a409ba29c48d13fc579d67d29ad
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-09 11:10:57 +0530

    setting booking controller..

--- src/main/java/com/xynerotech/task/household_services_booking_platform/controller/BookingController.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/controller/BookingController.java
@@ -1,4 +1,40 @@
 package com.xynerotech.task.household_services_booking_platform.controller;
 
+
+import com.xynerotech.task.household_services_booking_platform.dto.bookingDTO.RequestBookingDTO;
+import com.xynerotech.task.household_services_booking_platform.dto.bookingDTO.ResponseBookingDTO;
+import com.xynerotech.task.household_services_booking_platform.entities.Booking;
+import com.xynerotech.task.household_services_booking_platform.response.ApiResponse;
+import com.xynerotech.task.household_services_booking_platform.service.BookingService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("api/user")
 public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+
+    @PostMapping("/{userid}/bookings/add")
+    public ResponseEntity<ApiResponse<ResponseBookingDTO>> addBooking(@PathVariable Long userid,@Valid @RequestBody RequestBookingDTO requestBookingDTO) {
+        Booking booking = bookingService.addBooking(userid,requestBookingDTO);
+        ResponseBookingDTO responseBookingDTO = ResponseBookingDTO.bookingToDto(booking);
+
+        ApiResponse<ResponseBookingDTO> response = new ApiResponse<>(
+                "Booking successfully added",
+                LocalDateTime.now(),
+                responseBookingDTO,
+                true,
+                HttpStatus.CREATED.value()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
