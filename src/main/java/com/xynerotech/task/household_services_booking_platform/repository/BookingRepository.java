1c37241b01df4797f9c19aff9ddeb85a7e530c0b
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-09 09:01:28 +0530

    jpa method for fetching booking.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/repository/BookingRepository.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/repository/BookingRepository.java
@@ -1,7 +1,13 @@
 package com.xynerotech.task.household_services_booking_platform.repository;
 
+import com.xynerotech.task.household_services_booking_platform.entities.AppUser;
 import com.xynerotech.task.household_services_booking_platform.entities.Booking;
+import com.xynerotech.task.household_services_booking_platform.entities.HomeService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface BookingRepository  extends JpaRepository<Booking,Long> {
+
+    boolean existsByUserAndServiceAndBookingDate(AppUser user, HomeService service, LocalDate bookingDate);
 }
