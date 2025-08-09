127ec71a3f48cb3d8e1c35ea17cf1eb0347408f5
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-09 11:14:09 +0530

    method spec.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingService.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingService.java
@@ -1,11 +1,13 @@
 package com.xynerotech.task.household_services_booking_platform.service;
 
-import com.xynerotech.task.household_services_booking_platform.dto.bookingDTO.CreateBookingDTO;
+import com.xynerotech.task.household_services_booking_platform.dto.bookingDTO.RequestBookingDTO;
 import com.xynerotech.task.household_services_booking_platform.entities.Booking;
 
 public interface BookingService {
 
-    Booking addBooking(CreateBookingDTO bookingDTO);
+    Booking addBooking(Long userId,RequestBookingDTO bookingDTO);
+
+
 
 
 }
