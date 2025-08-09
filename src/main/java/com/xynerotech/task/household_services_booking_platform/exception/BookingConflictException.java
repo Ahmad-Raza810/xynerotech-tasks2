235b41e3ef4d6abdf94ab1a82f04e1a01ec94503
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-09 08:59:39 +0530

    add booking conflic exception.

---
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/exception/BookingConflictException.java
@@ -0,0 +1,7 @@
+package com.xynerotech.task.household_services_booking_platform.exception;
+
+public class BookingConflictException extends RuntimeException {
+    public BookingConflictException(String message) {
+        super(message);
+    }
+}
