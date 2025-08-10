dcb041df24c08349c81ab0b62c6f693e8fad33e2
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-10 10:17:58 +0530

    get my booking endpoint.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/controller/BookingController.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/controller/BookingController.java
@@ -10,9 +10,13 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("api/user")
@@ -22,9 +26,11 @@ public class BookingController {
     private BookingService bookingService;
 
 
+    //Route for making a booking
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/{userid}/bookings/add")
-    public ResponseEntity<ApiResponse<ResponseBookingDTO>> addBooking(@PathVariable Long userid,@Valid @RequestBody RequestBookingDTO requestBookingDTO) {
-        Booking booking = bookingService.addBooking(userid,requestBookingDTO);
+    public ResponseEntity<ApiResponse<ResponseBookingDTO>> addBooking(@PathVariable Long userid, @Valid @RequestBody RequestBookingDTO requestBookingDTO) {
+        Booking booking = bookingService.addBooking(userid, requestBookingDTO);
         ResponseBookingDTO responseBookingDTO = ResponseBookingDTO.bookingToDto(booking);
 
         ApiResponse<ResponseBookingDTO> response = new ApiResponse<>(
@@ -37,4 +43,32 @@ public class BookingController {
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+
+    //Route for "Get My booking"
+    @GetMapping("/{userid}/bookings")
+    public ResponseEntity<ApiResponse<List<ResponseBookingDTO>>> getMyBooking(
+            @PathVariable("userid") Long userId,
+            Authentication authentication) {
+
+        List<Booking> bookings=bookingService.getBookingsByUserId(userId,authentication);
+
+        List<ResponseBookingDTO> bookingDTOS = bookings
+                .stream()
+                .map(ResponseBookingDTO::bookingToDto)
+                .toList();
+
+        String message = bookingDTOS.isEmpty() ? "There is no bookings." : "Booking fetched successfully.";
+
+        ApiResponse<List<ResponseBookingDTO>> response = new ApiResponse<>(
+                message,
+                LocalDateTime.now(),
+                bookingDTOS,
+                true,
+                HttpStatus.OK.value()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
