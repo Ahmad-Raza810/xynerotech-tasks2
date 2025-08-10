51d6ca31e3503146f466910d3f5940f2be3ce7b6
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-10 10:25:23 +0530

    add new method spec..

--- src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingService.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/service/BookingService.java
@@ -2,10 +2,18 @@ package com.xynerotech.task.household_services_booking_platform.service;
 
 import com.xynerotech.task.household_services_booking_platform.dto.bookingDTO.RequestBookingDTO;
 import com.xynerotech.task.household_services_booking_platform.entities.Booking;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
 
 public interface BookingService {
 
     Booking addBooking(Long userId,RequestBookingDTO bookingDTO);
+
+    List<Booking> getAllBookings();
+
+    List<Booking> getBookingsByUserId(Long userId,Authentication authentication);
+
 
 
 
