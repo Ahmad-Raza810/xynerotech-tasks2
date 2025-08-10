4e3f66257237d953c117aa6c6be598a5d289bc44
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-10 23:05:56 +0530

    removing unnecessary fields fro dto.

--- src/main/java/com/xynerotech/task/household_services_booking_platform/dto/bookingDTO/ResponseBookingDTO.java
+++ src/main/java/com/xynerotech/task/household_services_booking_platform/dto/bookingDTO/ResponseBookingDTO.java
@@ -1,7 +1,6 @@
 package com.xynerotech.task.household_services_booking_platform.dto.bookingDTO;
 
 import com.xynerotech.task.household_services_booking_platform.entities.Booking;
-import com.xynerotech.task.household_services_booking_platform.entities.BookingStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,8 +20,6 @@ public class ResponseBookingDTO {
 
     private LocalDate bookingDate;
 
-    private BookingStatus status;
-
 
     public static ResponseBookingDTO bookingToDto(Booking booking){
         ResponseBookingDTO bookingDTO=new ResponseBookingDTO();
@@ -30,8 +27,6 @@ public class ResponseBookingDTO {
         bookingDTO.setUserId(booking.getUser().getUserId());
         bookingDTO.setServiceId(booking.getService().getId());
         bookingDTO.setBookingDate(booking.getBookingDate());
-        bookingDTO.setStatus(booking.getStatus());
-
         return bookingDTO;
     }
 }
